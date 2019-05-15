48455fc574cd4f6f114c33b45d758fb9393a174b
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-05-14 22:23:31 -0300

    Adicionei o projeto ao repositório

---
+++ src/PacoteDB/Conexao.java
@@ -0,0 +1,26 @@
+package PacoteDB;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexao {
+
+    public static Connection abrirConexao() {
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.jdbv.Driver").newInstance();
+            String url = "jdbc:mysql://127.0.0.1/estacionamento?";
+            url += "user=root&password=";
+
+            con = DriverManager.getConnection(url);
+            System.out.println("Conexão aberta!");
+        } catch (SQLException e) {
+        } catch(ClassNotFoundException e){
+        } catch(Exception e){
+        }
+        return con;
+    }
+}
---
+++ src/projetodatabase/ProjetoDatabase.java
@@ -0,0 +1,16 @@
+package projetodatabase;
+
+/**
+ *
+ * @author LABORATORIO_INFO
+ */
+public class ProjetoDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
